30ed0c050727d38a65f2672c2b312d9803338d90
Gian Bisma <gian.bisma.ph@gmail.com>
2021-01-05 23:36:33 +0700

    input

--- lab0/vstring.cpp
+++ lab0/vstring.cpp
@@ -1,7 +1,11 @@
 #include "vstring.hpp"
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-  printf("Hello World!\n");
+  string age;
+  cout << "Enter age: ";
+  cin >> age;
+  cout << "You are " << age << " years-old\n";
   return 0;
 }
